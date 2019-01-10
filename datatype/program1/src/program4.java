c67173e1de0d62515eb8330c16d6ba576a43d9f1
chayangulati321 <chayangulati12@gmail.com>
2019-01-10 13:17:51 +0530

    datatype

---
+++ datatype/program1/src/program4.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class program4
+{
+	public static void main(String[] args)
+	{
+		 Scanner input = new Scanner(System.in);
+        double minutesInYear = 60 * 24 * 365;
+        System.out.print("Input the number of minutes: ");
+        double min = input.nextDouble();
+        long years = (long) (min / minutesInYear);
+        int days = (int) (min / 60 / 24) % 365;
+        System.out.println((int) min + " minutes is approximately " + years + " years and " + days + " days");
+    }
+}
