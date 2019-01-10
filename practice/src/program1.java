dfb6043d6b35a27dba359e9fababf4059c4c242f
chayangulati321 <chayangulati12@gmail.com>
2019-01-10 22:06:55 +0530

    controlstatement

---
+++ practice/src/program1.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class program1
+{
+	public static void main(String[] args)
+	{
+		int a=4;
+		int b=2;
+		int Add;
+		int Sub;
+		int Mul;
+		int Div;
+		int Mod;
+		Add=a+b;
+		Sub=a-b;
+		Mul=a*b;
+		Div=a/b;
+		Mod=a%b;
+		System.out.println("Addition :"+ Add);
+		System.out.println("Subtraction :"+ Sub);
+		System.out.println("Multiplication :"+ Mul);
+		System.out.println("Division :"+ Div);
+		System.out.println("Modulus :"+ Mod);
+	}
+}
